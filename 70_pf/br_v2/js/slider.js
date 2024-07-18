54def26b6c6eab63722836d99551577c55f0aeeb
mayummi <8838730@naver.com>
2024-07-18 17:06:35 +0900

    0718

--- 70_pf/br_v2/js/slider.js
+++ 70_pf/br_v2/js/slider.js
@@ -10,41 +10,71 @@
     플러그인은 어떤 특정한 하나의 문제를 해결하기 위한 컴포넌트(component)이다. 예) bx슬라이더
 */
 
+
+// 바닐라 스크립트
+// window.addEventListener("DOMContentLoaded", function(){
+//     // 문서가 준비되면 실행할 코드;
+// });
+
+
 // 제이쿼리
-$('.bxslider').bxSlider({
-    // mode: "horizontal"(기본값), "fade", "vertical"
-    //mode: "fade",
-    // speed: 500(기본값), 밀리초
-    //speed: 1000,
-    // slieMargin: 0
-    // https://gsgd.co.uk/sandbox/jquery/easing/ 
-    // easing: null
-    // pager: true(기본값) - pagination, indicator라고 부른다.
-    pager: true,
-    // pagerType: 'full'(기본값), 'short'
-    pagerType: 'short',
-    // pagerShortSeparator: '/'(기본값)
-    pagerShortSeparator: ' | ',
-    // controls: true (기본값) -> next/prev 버튼
-    //controls: false, 
-    // play/stop 버튼 표시
-    autoControls: false,
-    // 자동 슬라이드
-    auto: true,
-    // 클릭시 자동 슬라이드 정지
-    stopAutoOnClick: true,
-    // pause: 4000 (기본값) -> 정지 시간
-    //pause: 1000,
-    // autoStart: true(기본값) -> 자동 슬라이드
-    autoStart: true,
-    // autoDirection: 'next'(기본값) -> 슬라이드 방향
-    // next이면 오른쪽에서 왼쪽 방향
-    // prev이면 왼쪽에서 오른쪽 방향
-    // autoDirection: 'prev',
-    // autoHover: false(기본값), true이면 슬라이드에 호버시 멈춤
-    autoHover: true,
-    // 슬라이드 크기(단, px 단위 생략)
-    // autoDelay: 0(기본값) -> 자동 슬라이드 시작 시 지연 시간
-    autoDelay: 4000
-    // slideWidth: 1903
-});
+// $(document).click(function(){});
+// $(document).mouseenter(function(){});
+// $(document).mouseleave(function(){});
+$(document).ready(function () {
+    // 문서가 준비되면 실행할 코드;
+
+
+    $('.bxslider').bxSlider({
+
+        ///////////////////////////////////////////////////////////////////
+
+
+        // mode: "horizontal"(기본값), "fade", "vertical"
+        //mode: "fade",
+        // speed: 500(기본값), 밀리초
+        //speed: 1000,
+        // slideMargin: 0,
+        // slideMargin: 0
+        // https://gsgd.co.uk/sandbox/jquery/easing/ 
+        // easing: null
+
+        ///////////////////////////////////////////////////////////////
+
+
+        // pager: true(기본값) - pagination, indicator라고 부른다.
+        // pager: true,
+        // pagerType: 'full'(기본값), 'short'
+        // pagerType: 'short',
+        // pagerShortSeparator: '/'(기본값)
+        // pagerShortSeparator: ' | ',
+
+        //////////////////////////////////////////////////
+        // controls: true (기본값) -> next/prev 버튼
+        //controls: false, 
+        // play/stop 버튼 표시
+        autoControls: false,
+
+        //****************************************************/
+
+        // auto: true(기본값) -> 자동 전환
+        auto: true,
+        // 클릭시 자동 슬라이드 정지
+        stopAutoOnClick: true,
+        // pause: 4000 (기본값) -> 일시정지 시간
+        //pause: 1000,
+        // autoStart: true(기본값) -> 자동 슬라이드
+        autoStart: true,
+        // autoDirection: 'next'(기본값) -> 슬라이드 방향
+        // next이면 오른쪽에서 왼쪽 방향
+        // prev이면 왼쪽에서 오른쪽 방향
+        // autoDirection: 'prev',
+        // autoHover: false(기본값), true이면 슬라이드에 호버시 멈춤
+        autoHover: true,
+        // 슬라이드 크기(단, px 단위 생략)
+        // autoDelay: 0(기본값) -> 자동 슬라이드 시작 시 지연 시간
+        //autoDelay: 4000
+        // slideWidth: 1903
+    });
+
+}); // document.ready()
