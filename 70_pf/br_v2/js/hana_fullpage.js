254142fcfb27fc98de3b74714a6230c3c97b08e0
mayummi <8838730@naver.com>
2024-07-19 12:21:13 +0900

    Update hana_fullpage.js

--- 70_pf/br_v2/js/hana_fullpage.js
+++ 70_pf/br_v2/js/hana_fullpage.js
@@ -1,12 +1,5 @@
 // Hanatour fullpage.js
 
-$(document).ready(function() {
-    $('#fullpage').fullpage({
-       //options here
-       autoScrolling:true,
-       scrollHorizontally: true
-    });
- 
-    //methods
-    $.fn.fullpage.setAllowScrolling(false);
- });
+$(document).ready(function () {
+    $('#fullpage').fullpage();
+});
