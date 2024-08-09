f9beed7586e6fab02faf2d09012bb636ab4df23b
mayummi <8838730@naver.com>
2024-08-09 10:12:02 +0900

    Update auto_commit.bat

--- auto_commit.bat
+++ auto_commit.bat
@@ -1,18 +1,18 @@
 :loop
 
-   cd C:\Users\USER\Documents\GitHub\ju-ing
+    cd F:\마유미\web0422
 
-   git init
-   
-   git pull
-   
-   git add --all
-   
-   git commit -m "auto commit"
-   
-   git push
-   
-   :: 3600초 대기
-   TIMEOUT 3600
-   
+    git init
+    
+    git pull
+    
+    git add --all
+    
+    git commit -m "auto commit"
+    
+    git push
+    
+    :: 3600초 대기
+    TIMEOUT 3600
+
 goto loop
