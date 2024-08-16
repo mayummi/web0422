e199e25b95b157d802e732074b607958cdbe7cb8
mayummi <8838730@naver.com>
2024-08-16 16:10:54 +0900

    auto commit

--- 70_pf/we_jq/script/script.js
+++ 70_pf/we_jq/script/script.js
@@ -1,4 +1,29 @@
 $(()=>{
+
+    // 슬라이드 이미지
+    let idx = 0;
+            setInterval(autoSlide, 2000);
+            
+            function autoSlide() {
+                (idx === 2) ? idx = 0 : idx++;
+                // fatd- 
+                // $("#slide li").eq(idx).animate({opacity: 1 },1000,function(){
+                //     if (idx === 0) idx = 3;
+                //     $("#slide li").eq(idx-1).animate({opacity: 0}, 1000);
+                //     if (idx === 3) idx = 0;
+                // });
+
+                // 1in, 0out -> 2in, 1out -> 0in, 2out
+                $("#slide li").eq(idx).fadeIn(1000,function(){
+                    if (idx === 0) idx = 3;
+                    $("#slide li").eq(idx-1).fadeOut(1000);
+                    if (idx === 3) idx = 0;
+                });
+            };
+
+
+
+
     //////////////////////////// 모달 팝업 ///////////////////
     // 요소 찾기
     // const firstList = $("#notice a").eq(0);
