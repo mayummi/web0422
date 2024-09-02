1a483b856946420ad4219e1c9b86aee996d33bad
mayummi <8838730@naver.com>
2024-09-02 12:34:00 +0900

    0902

--- 90_test/ReactJS_마유미/src/Card.js
+++ 90_test/ReactJS_마유미/src/Card.js
@@ -15,12 +15,12 @@ function Card(props){
             </div>
             <div className="card_btn">
                 {/* <button>{props.mheart}</button> */}
-                <p>40% <span>5,900 원</span></p>
+                <p><b>40%</b> <span>5,900 원</span></p>
                 {/* <a href="#">구매</a> */}
             </div>
             <div className="dr">
-                <span className="card_rate"><img className="rate_img" src={props.rate_img} alt={props.rate_img} />{props.mrate}</span>
-                <span className="card_date"><img className="co_img" src={props.co_img} alt={props.co_img} />{props.mdate}</span>
+                <span className="card_rate"><img className="rate_img" src={props.rate_img} alt={props.rate_img} /> {props.mrate} </span>
+                <span className="card_date"><img className="co_img" src={props.co_img} alt={props.co_img} /> {props.mdate}</span>
             </div>
         </li>
     );
