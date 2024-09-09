de9c4ab73bc1280cc5c367542d6aa72101db6b63
mayummi <8838730@naver.com>
2024-09-09 09:44:58 +0900

    0909

---
+++ 70_pf/ottogi/js/script.js
@@ -0,0 +1,23 @@
+
+
+// 탭
+const tabItem = document.querySelectorAll(".tab__item");
+const tabContent = document.querySelectorAll(".tab__content");
+
+tabItem.forEach((item, index) => {
+    // 3. 탭 버튼에 클릭 이벤트를 준다.
+    item.addEventListener("click", (e) => {
+        e.preventDefault(); // a 
+
+        tabContent.forEach((content) => {
+            content.classList.remove("active");
+        });
+
+        tabItem.forEach((content) => {
+            content.classList.remove("active");
+        });
+
+        tabItem[index].classList.add("active");
+        tabContent[index].classList.add("active");
+    });
+});
