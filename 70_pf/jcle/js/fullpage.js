5a6c6c7c561f43e220c41927cf35b4d55da3c85d
mayummi <8838730@naver.com>
2024-10-16 18:12:53 +0900

    1016

---
+++ 70_pf/jcle/js/fullpage.js
@@ -0,0 +1,14 @@
+$(function(){
+
+    $('#wrap').fullpage({
+        // menu: '#hana_nav',
+        // anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'lastPage'],
+        // sectionsColor: ['#f2f2f2', '#4BBFC3', '#7BAABE', 'whitesmoke'],
+        // navll
+        navigation: true,
+        navigationTooltips: ['Home', 'Best Tour', 'Favorit', 'Culture'],
+        showActiveTooltip: true,
+        //slidesNavigation: true,
+        //loopHorizontal: false,
+    });
+});
