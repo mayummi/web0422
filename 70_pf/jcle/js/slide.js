4ee357897f43d3e4febe43098ca0ee96b42a5388
mayummi <8838730@naver.com>
2024-10-21 23:13:31 +0900

    1021

---
+++ 70_pf/jcle/js/slide.js
@@ -0,0 +1,11 @@
+$(function () {
+    $('.slider').bxSlider({
+        autoControls: false,
+
+        auto: true,
+        stopAutoOnClick: true,
+        autoStart: true,
+        autoHover: true,
+    });
+
+}); // document.ready()
